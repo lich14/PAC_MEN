92abd0011b7ed6714d105a6407daad0060af1b33
lich14 <987743919@qq.com>
2020-12-23 14:58:32 +0800

    init

--- pac_men/__init__.py
+++ pac_men/__init__.py
@@ -1,6 +1,6 @@
 import gym
 import itertools
-from .pac_men import CustomEnv
+from pac_men.pac_men import CustomEnv
 
 _sizes = {
     "tiny": (1, 3, 5),
@@ -8,8 +8,18 @@ _sizes = {
 
 gym.register(
     id=f"pacmen-tiny-4ag-v0",
-    entry_point="pac_men.pac_men:CustomEnv",
+    entry_point="pac_men:CustomEnv",
     kwargs={
         "n_agents": 4,
     },
 )
+
+
+def full_registration():
+    gym.register(
+        id=f"pacmen-tiny-4ag-v0",
+        entry_point="pac_men:CustomEnv",
+        kwargs={
+            "n_agents": 4,
+        },
+    )
